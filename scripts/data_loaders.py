9e5b92925bd5ba5edb452a5374a9364c0f9be71e
Frederico Zolio <91195110+fredzolio@users.noreply.github.com>
2024-08-24 15:05:52 -0300

    Adaptação da forma como o data_loaders carrega as credenciais para facilitar o deploy no StreamLit

--- scripts/data_loaders.py
+++ scripts/data_loaders.py
@@ -6,10 +6,22 @@ import os
 load_dotenv()
 
 def run_query(query):
-    credentials = service_account.Credentials.from_service_account_file(os.getenv('CREDENTIALS_PATH'))
-    client = bigquery.Client(credentials=credentials, project="datascientist-fz")
-    
-    
-    df = client.query(query).to_dataframe(geography_as_object=True,progress_bar_type='tqdm')
-    
+    credentials = service_account.Credentials.from_service_account_info({
+        "type": os.getenv('TYPE'),
+        "project_id": os.getenv('PROJECT_ID'),
+        "private_key_id": os.getenv('PRIVATE_KEY_ID'),
+        "private_key": os.getenv('PRIVATE_KEY').replace('\\n', '\n'),
+        "client_email": os.getenv('CLIENT_EMAIL'),
+        "client_id": os.getenv('CLIENT_ID'),
+        "auth_uri": os.getenv('AUTH_URI'),
+        "token_uri": os.getenv('TOKEN_URI'),
+        "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER_X509_CERT_URL'),
+        "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL'),
+        "universe_domain": os.getenv('UNIVERSE_DOMAIN')
+    })
+
+    client = bigquery.Client(credentials=credentials, project=os.getenv('PROJECT_ID'))
+
+    df = client.query(query).to_dataframe(geography_as_object=True, progress_bar_type='tqdm')
+
     return df
